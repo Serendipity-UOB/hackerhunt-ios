eeb9ade6695ec3ac9b61a132787945f2bba22dfc
Louis Heath <lheath1998@gmail.com>
2018-11-21 14:24:56 +0000

    init

---
+++ hackerhunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hackerhunt
+//
+//  Created by Louis Heath on 21/11/2018.
+//  Copyright © 2018 Louis Heath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ hackerhuntTests/hackerhuntTests.swift
@@ -0,0 +1,34 @@
+//
+//  hackerhuntTests.swift
+//  hackerhuntTests
+//
+//  Created by Louis Heath on 21/11/2018.
+//  Copyright © 2018 Louis Heath. All rights reserved.
+//
+
+import XCTest
+@testable import hackerhunt
+
+class hackerhuntTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
