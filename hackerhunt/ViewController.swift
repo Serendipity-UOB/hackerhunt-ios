584da6bddccee3512ffbe3462a69a0c912cc9e4f
Louis Heath <lheath1998@gmail.com>
2018-11-23 12:34:04 +0000

    ranging beacons and displaying rssi

--- hackerhunt/ViewController.swift
+++ hackerhunt/ViewController.swift
@@ -13,32 +13,36 @@ class ViewController: UIViewController {
   
     var beaconManager: KTKBeaconManager!
     
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var infoLbl: UILabel!
+    @IBOutlet weak var countLbl: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = "starting"
+        infoLbl.text = "starting"
+        countLbl.text = ""
         
         // Initiate Beacon Manager
         beaconManager = KTKBeaconManager(delegate: self)
         
+        if (KTKBeaconManager.locationAuthorizationStatus() == .notDetermined) {
+            beaconManager.requestLocationAlwaysAuthorization()
+        }
+        
         switch KTKBeaconManager.locationAuthorizationStatus() {
-            case .notDetermined:
-                label.text = "authorising?"
-                beaconManager.requestLocationAlwaysAuthorization()
-                break
-            case .denied, .restricted, .authorizedWhenInUse:
-                label.text = "started"
-                break
-            case .authorizedAlways:
-                label.text = "authorised"
-                break
+        case .notDetermined:
+            break
+        case .denied, .restricted:
+            infoLbl.text = "unauthorised"
+            break
+        case .authorizedWhenInUse, .authorizedAlways:
+            infoLbl.text = ""
+            break
         }
         
         // Region
         let proximityUUID = NSUUID(uuidString: "f7826da6-4fa2-4e98-8024-bc5b71e0893e")
-        let region = KTKBeaconRegion(proximityUUID: proximityUUID! as UUID, identifier: "default-region")
+        let region = KTKBeaconRegion(proximityUUID: proximityUUID! as UUID, identifier: "mvb")
         
         // Start Monitoring and Ranging
         beaconManager.startMonitoring(for: region)
@@ -49,36 +53,49 @@ class ViewController: UIViewController {
 
 extension ViewController: KTKBeaconManagerDelegate {
     
+    func proximityFrom(enum p: CLProximity) -> String {
+        var proximities = ["unknown", "immediate", "near", "far"]
+        return proximities[p.rawValue]
+    }
+    
+    func beaconNameFrom(major m: NSNumber) -> String {
+        var majorToName = [ "", "4VSu", "7Wuj", "gHm1", "20LC" ]
+        return majorToName[m.intValue]
+    }
+    
     func beaconManager(_ manager: KTKBeaconManager, didRangeBeacons beacons: [CLBeacon], in region: KTKBeaconRegion) {
-        print("Did ranged \"\(beacons.count)\" beacons")
-        label.text = "Did ranged \"\(beacons.count)\" beacons"
-    }
-    
-    func beaconManager(_ manager: KTKBeaconManager, didStartMonitoringFor region: KTKBeaconRegion) {
-        // Do something when monitoring for a particular
-        // region is successfully initiated
-        print("Started monitoring region \"\(region)\"")
-        label.text = "Started monitoring region \"\(region)\""
-    }
-    
-    func beaconManager(_ manager: KTKBeaconManager, monitoringDidFailFor region: KTKBeaconRegion?, withError error: NSError?) {
-        // Handle monitoring failing to start for your region
-        print("Failed to monitor region \"\(String(describing: region))\"")
-        label.text = "Failed to monitor region \"\(String(describing: region))\""
+        // show a list of beacons and their RSSI
+        let sorted = beacons.sorted(by: { $0.major.compare($1.major) == .orderedAscending })
+        var text = ""
+        for beacon in sorted {
+            text += "beacon: \(beaconNameFrom(major: beacon.major)), rssi: \(beacon.rssi)\nprox: \(proximityFrom(enum: beacon.proximity)), accuracy: \(Double(round(100*beacon.accuracy)/100))\n\n"
+        }
+        countLbl.text = text
     }
     
     func beaconManager(_ manager: KTKBeaconManager, didEnter region: KTKBeaconRegion) {
-        // Decide what to do when a user enters a range of your region; usually used
-        // for triggering a local notification and/or starting a beacon ranging
-        print("Entered region \"\(region)\"")
-        label.text = "Entered region \"\(region)\""
+        // Entered range of region
+        //   Player is within MVB
+        print("Entered beacon range: \(region)")
+        infoLbl.text = "Entered MVB"
     }
     
     func beaconManager(_ manager: KTKBeaconManager, didExitRegion region: KTKBeaconRegion) {
-        // Decide what to do when a user exits a range of your region; usually used
-        // for triggering a local notification and stoping a beacon ranging
-        print("Exited region \"\(region)\"")
-        label.text = "Exited region \"\(region)\""
+        // Entered range of region
+        //   Player has left MVB, notify player and server?
+        print("Exited beacon range: \(region)")
+        infoLbl.text = "Not in MVB"
     }
     
+    /*
+     These don't seem to get hit. We also don't really need them but leave in for debug
+     */
+    
+    func beaconManager(_ manager: KTKBeaconManager, didStartMonitoringFor region: KTKBeaconRegion) {
+        print("Started monitoring region \"\(region)\"")
+    }
+    
+    func beaconManager(_ manager: KTKBeaconManager, monitoringDidFailFor region: KTKBeaconRegion?, withError error: Error?) {
+        print("Failed to monitor region \"\(String(describing: region))\"")
+    }
 }
